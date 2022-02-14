f58177eae45d82c6d3a962b92819102443e30c01
neqkir <89974426+neqkir@users.noreply.github.com>
2022-02-14 14:02:31 +0100

    Update wikisource-get-book.py

--- wikisource-get-book.py
+++ wikisource-get-book.py
@@ -9,62 +9,96 @@ from bs4 import BeautifulSoup
 import requests
 import tqdm
 
-BOOK_TO_SCRAP = "Micromégas"
+LANGUAGE = "en"
 
+BOOK_TO_SCRAP = "Micromégas"
 # Other examples
-#BOOK_TO_SCRAP = "Merchant_of_Venice_(1923)_Yale"
+BOOK_TO_SCRAP = "Merchant_of_Venice_(1923)_Yale"
 #BOOK_TO_SCRAP = "Poésies_(Mallarmé,_1914,_8e_éd.)"
 
 WITH_SUBTITLES = True
 
+# Exlude specific pages (page name starting with)
+EXCLUDE = ["Appendix", "Index", "Bibliography", "Notes" ]
+
+NO_FOOT_NOTES = True
+
 
 def get_content_page(url):
+
+    print(url)
 
     res = urllib.request.urlopen(url)
     wiki = BeautifulSoup(res, "lxml")
 
     title = wiki.title.string
-    
+
     text = ""
     elems = wiki.select('p')
+
     
     for elem in elems:
-        text += re.sub(r"\n", " ", elem.getText())
+
+        if NO_FOOT_NOTES and (elem.text.startswith("Footnotes")):
+            break
+
+        text += re.sub(r"\n", " ", elem.text)
         text += "\n"
 
     text = re.sub(r"(\[\d+\])+", "", text)
 
     return title, text
 
-def get_book_urls(lang, url_title):
+
+
+def get_book_urls(url_title):
     '''
     For a given wikisource book, e.g.,
     https://en.wikisource.org/wiki/Merchant_of_Venice_(1923)_Yale
     fetches all urls for book parts (chapters, acts etc.)
     '''
+    
     pages = []
 
-    url = "https://" + lang + ".wikisource.org/wiki/" + url_title
+    url = "https://" + LANGUAGE + ".wikisource.org/wiki/" + url_title
     req = urllib.request.urlopen(url)
     wiki = BeautifulSoup(req, "lxml")
 
     for link in wiki.find(id="bodyContent").findAll('a'):
-         
-        if link['href'].find("/wiki/"+url_title) == -1:
+
+        # exclude everything non wikisource text page
+        
+        excl1 = link['href'].find("/wiki/"+url_title) 
+        excl2 = link['href'].find("/wiki/"+BOOK_TO_SCRAP)
+
+        if excl1 == -1 and excl2 == -1:
             continue
-        else:
-            print(str(link.get('href')))
-            pages.append("https://fr.wikisource.org/" + str(link.get('href')))
+
+        # exclude pages corresponding to elements in EXCLUDE
+        # e.g., appendix, notes, bibliography etc.
+        # e.g, if EXCLUDE = ["appendix", "notes"] for https://en.wikisource.org/wiki/Merchant_of_Venice_(1923)_Yale
+        # it will exclude pages whose name (after /wiki/Merchant_of_Venice_(1923)_Yale/) starts with appendix or notes
+        
+
+        exclude_list = ["/wiki/" + url_title.lower() + "/" + i.lower() for i in EXCLUDE]+\
+                       ["/wiki/" + BOOK_TO_SCRAP.lower() + "/" + i.lower() for i in EXCLUDE]
+
+        if any(elem in link['href'].lower() for elem in exclude_list):
+            continue
+
+        #print(str(link.get('href')))         
+        pages.append("https://" + LANGUAGE + ".wikisource.org/" + str(link['href']))
 
     #print(pages)
         
     return pages
     
+
     
-def get_book(lang, url_title):
+def get_book(url_title):
     
     content = ""
-    url = "https://" + lang + ".wikisource.org/wiki/" + url_title
+    url = "https://" + LANGUAGE + ".wikisource.org/wiki/" + url_title
     req = urllib.request.urlopen(url)
     wiki = BeautifulSoup(req, "lxml")
 
@@ -75,17 +109,22 @@ def get_book(lang, url_title):
 
     # Get all urls (for parts, chapters, acts etc.)
 
-    urls = get_book_urls(lang, url_title)
+    urls = get_book_urls(url_title)
     pbar = tqdm.tqdm(urls)
 
-    for url in pbar:  
+    for url in pbar:
+        
         page_title, page_content = get_content_page(url) 
+
         if WITH_SUBTITLES:
             content += "\n" + page_title + "\n"
+
         content += page_content + " "
         pbar.set_description("Processing %s" % url.split('/')[-2])
         
     return title, content
+
+
     
 def save_to_file(text, title):
     f = '":.*<>|/\\?'
@@ -99,9 +138,10 @@ def save_to_file(text, title):
     return
 
 
+
 if __name__ == "__main__":
 
     url_title = urllib.parse.quote(BOOK_TO_SCRAP)
-    titre, livre = get_book("fr", url_title)
+    titre, livre = get_book(url_title)
     save_to_file(livre, titre)
 
